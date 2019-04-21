4a8c41054a46383801745fb376cd86385911810b
Mario R <mariort@outlook.com>
2019-04-21 20:32:31 +0200

    add sample controller

---
+++ src/Controller/SampleController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use OpenApi\Annotations as OA;
+
+class SampleController
+{
+    /**
+     * @Route("/v1/", methods={"GET"})
+     *
+     * @OA\Get(
+     *      path="/v1/",
+     *      @OA\Response(response=204, ref="#/components/responses/NoContent"),
+     *      @OA\Response(response=400, ref="#/components/responses/BadRequest"),
+     *      @OA\Response(response=500, ref="#/components/responses/InternalError")
+     * )
+     */
+    public function get()
+    {
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
+    }
+}
