05ca6056dc8b0b8df704c1c6ed19b2ec8645881d
Mario R <mariort@outlook.com>
2019-04-21 19:25:09 +0200

    add phpunit

---
+++ tests/SampleTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    public function testAdd()
+    {
+        $this->assertEquals(true, true);
+    }
+}
